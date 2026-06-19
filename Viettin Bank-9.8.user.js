be0aa8eea5944d2122a32d441017346d4b9938b4
chungdang-hub <chung.dang@techcoop.vn>
2026-06-19 11:35:26 +0700

    Update Viettin Bank-9.8.user.js

--- Viettin Bank-9.8.user.js
+++ Viettin Bank-9.8.user.js
@@ -4,6 +4,8 @@
 // @version      9.8
 // @description  Tối ưu hóa hiệu năng, sửa lỗi vòng lặp DOM và nâng cấp độ chính xác quét dữ liệu VietinBank điền vào Odoo.
 // @author       NGOCCHUNG
+// @downloadURL  https://raw.githubusercontent.com/chungdang-hub/bank-userscripts/edit/main/Viettin%20Bank-9.8.user.js
+// @updateURL    https://raw.githubusercontent.com/chungdang-hub/bank-userscripts/edit/main/Viettin%20Bank-9.8.user.js
 // @match        *://farmlink.techcoop.vn/*
 // @grant        none
 // @run-at       document-start
